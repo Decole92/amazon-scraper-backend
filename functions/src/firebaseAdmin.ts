fea0517d57cbc5309f8cbc8f58e90d7d3c1a5ff7
Decole mills <okpoka92@gmail.com>
2023-03-09 11:14:48 +0100

    modifying

--- functions/src/firebaseAdmin.ts
+++ functions/src/firebaseAdmin.ts
@@ -1,8 +1,9 @@
 import * as admin from "firebase-admin";
 import {getApps} from "firebase-admin/app";
-/* eslint @typescript-eslint/no-var-requires: "off" */
-const serviceAccount = require("../serviceAccountKey.json");
 
+/* eslint @typescript-eslint/no-var-requires: "off" */
+//const serviceAccount = require("../serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY as string);
 if (!getApps().length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
